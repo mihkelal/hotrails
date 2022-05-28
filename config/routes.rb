b7187dd541df7f419274e3101ddc76f5b26f03da
Mihkel Alavere <mihkel.alavere@eesti.ee>
2022-05-28 21:20:18 +0300

    Nested Turbo Frames (Setup)

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :quotes do
-    resources :line_item_dates, except: %i[index show]
+    resources :line_item_dates, except: %i[index show] do
+      resources :line_items, except: %i[index show]
+    end
   end
 end
